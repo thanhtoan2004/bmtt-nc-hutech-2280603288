c10384b83ab4a16e5aa59affe0c7b4f6a374a328
thanhtoan2004 <thanhtoan06092004@gmail.com>
2025-05-19 15:48:57 +0700

    update

---
+++ lab2/api.py
@@ -0,0 +1,119 @@
+from flask import Flask, request, jsonify
+from cipher.caesar import CeasarCipher
+from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailFenceCipher
+from cipher.Playfair import PlayFairCipher
+from cipher.transposition import TranspositionCipher
+app = Flask(__name__)
+
+Ceasar_cipher = CeasarCipher()
+
+@app.route("/api/Caesar/encrypt", methods=["POST"])
+def ceasar_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = Ceasar_cipher.encrypt_text(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+
+@app.route("/api/Caesar/decrypt", methods=["POST"])
+def ceasar_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = Ceasar_cipher.decrypt_text(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
+
+vigenere_cipher = VigenereCipher()
+
+@app.route('/api/vigenere/encrypt', methods=["POST"])
+def vigenere_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+
+@app.route('/api/vigenere/decrypt', methods=["POST"])
+def vigenere_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
+
+
+railfence_cipher = RailFenceCipher()
+
+@app.route('/api/railfence/encrypt', methods=["POST"])
+def encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+
+@app.route('/api/railfence/decrypt', methods=["POST"])
+def decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
+
+
+playfair_cipher = PlayFairCipher()
+
+@app.route("/api/Playfair/creatematrix", methods=["POST"])
+def playfair_creatematrix():
+    data = request.json
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    return jsonify({"playfair_matrix": playfair_matrix})
+
+@app.route("/api/Playfair/encrypt", methods=["POST"])
+def playfair_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    encrypted_text = playfair_cipher.playfair_encrypt(plain_text, playfair_matrix)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route("/api/Playfair/decrypt", methods=["POST"])
+def playfair_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, playfair_matrix)
+    return jsonify({'decrypted_message': decrypted_text})
+
+
+#TRANSPOSITION CIPHER ALGORITHM
+transposition_cipher = TranspositionCipher()
+
+@app.route("/api/transposition/encrypt", methods=["POST"])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+@app.route("/api/transposition/decrypt", methods=["POST"])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+#main function
+if __name__ == "__main__":
+    app.run(host ="0.0.0.0", port=5000, debug=True)
